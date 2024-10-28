47e21df1fa470ecbaa4ba0fd1ec4be7ba0062e46
sspirig <santiago.sprg@eduge.ch>
2024-10-28 15:53:58 +0100

    dwa

---
+++ videorecorder/js/main.js
@@ -0,0 +1,26 @@
+"use strict";
+
+let stream = await navigator.mediaDevices.getUserMedia({video: true});
+
+const constraints = {
+    video: {
+    width: { ideal: 1280 },
+    height: { ideal: 720 },
+    facingMode: { exact: "environment" }
+    }
+};
+
+let cameraBox = document.querySelector("#cameraBox");
+let canvas = document.querySelector("#canvas");
+let btnTakePicture = document.querySelector("#btnShoot")
+cameraBox.srcObject = stream;
+
+canvas.width = cameraBox.videoWidth;
+canvas.height = cameraBox.videoHeight;
+
+btnTakePicture.addEventListener("click", () => {
+    canvas.width = cameraBox.videoWidth;
+    canvas.height = cameraBox.videoHeight;
+    canvas.getContext("2d").drawImage(cameraBox, 0, 0);
+    print(canvas.getDataURL());
+});
