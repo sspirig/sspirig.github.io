b55db8758cc5db5a9b77ad6520852ea0f946948b
sspirig <santiago.sprg@eduge.ch>
2024-11-04 14:04:11 +0100

    afda

--- geocaching/js/main.js
+++ geocaching/js/main.js
@@ -1,14 +1,14 @@
 "use strict";
 
-// let stream = await navigator.mediaDevices.getUserMedia({video: true});
+let stream = await navigator.mediaDevices.getUserMedia({video: true});
 
-// let constraints = {
-//     video: {
-//     width: { ideal: 1280 },
-//     height: { ideal: 720 },
-//     facingMode: { exact: "environment" }
-//     }
-// };
+let constraints = {
+    video: {
+    width: { ideal: 1280 },
+    height: { ideal: 720 },
+    facingMode: { exact: "environment" }
+    }
+};
 
 const map = L.map('map', {
     center: [-29.50, 145],
@@ -19,24 +19,24 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution:
 
 const marker1 = L.marker([-37.699450, 176.279420]).addTo(map);
 
-// let cameraBox = document.querySelector("#cameraBox");
-// let canvas = document.querySelector("#canvas");
-// let btnTakePicture = document.querySelector("#btnShoot");
-// let btnChangeCamera  = document.querySelector("#btnShoot");
-// cameraBox.srcObject = stream;
+let cameraBox = document.querySelector("#cameraBox");
+let canvas = document.querySelector("#canvas");
+let btnTakePicture = document.querySelector("#btnShoot");
+let btnChangeCamera = document.querySelector("#btnChange");
+cameraBox.srcObject = stream;
 
-// canvas.width = cameraBox.videoWidth;
-// canvas.height = cameraBox.videoHeight;
+canvas.width = cameraBox.videoWidth;
+canvas.height = cameraBox.videoHeight;
 
-// btnTakePicture.addEventListener("click", () => {
-//     canvas.width = cameraBox.videoWidth;
-//     canvas.height = cameraBox.videoHeight;
-//     canvas.getContext("2d").drawImage(cameraBox, 0, 0);
-//     print(canvas.getDataURL());
-// });
-// btnChangeCamera.addEventListener("click", () => {
-//     canvas.width = cameraBox.videoWidth;
-//     canvas.height = cameraBox.videoHeight;
-//     canvas.getContext("2d").drawImage(cameraBox, 0, 0);
-//     print(canvas.getDataURL());
-// });
+btnTakePicture.addEventListener("click", () => {
+    canvas.width = cameraBox.videoWidth;
+    canvas.height = cameraBox.videoHeight;
+    canvas.getContext("2d").drawImage(cameraBox, 0, 0);
+    print(canvas.getDataURL());
+});
+btnChangeCamera.addEventListener("click", () => {
+    canvas.width = cameraBox.videoWidth;
+    canvas.height = cameraBox.videoHeight;
+    canvas.getContext("2d").drawImage(cameraBox, 0, 0);
+    print(canvas.getDataURL());
+});
